a4db7256b720c9f3a4da087794854d50f25b7e23
Uzayr Akbar <uzayrakbar2306@gmail.com>
2026-06-05 23:44:47 +0100

    deleted stuff yk

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', fn() => view('welcome'));
 Route::get('/about', function (): never {
     dd('About page');
 });
-Route::get('/settigns', fn () => 'settings page');
+Route::get('/settigns', fn() => 'settings page');
